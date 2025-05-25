3bd94b106c73e69713ed8971fdf748f19a10c046
luckyluclucas <aristoteles0727@gmail.com>
2025-05-25 13:10:19 -0300

    update button theme for better ui

--- src/components/changeThemeButton.tsx
+++ src/components/changeThemeButton.tsx
@@ -3,22 +3,22 @@ import { useTheme } from "next-themes"
 import { MoonIcon } from "lucide-react"
 import { SunIcon } from "lucide-react"
 
-export default function ThemeButton(){
-    
-    const { theme, setTheme } = useTheme()
+export default function ThemeButton() {
 
-    if(theme !== 'light' && theme !== 'dark'){
-        return null
-    }
- 
-    return(
-      <button 
-      onClick={()=> theme == 'light' ? setTheme('dark'): setTheme('light')}
-      className={`absolute hover:cursor-pointer z-10 top-1 left-1 p-2 rounded-full ${theme == 'dark' ? 'bg-slate-900/50': 'bg-slate-900/50'}`}>
-        { theme == 'light' ?
-        <MoonIcon color="white" fill="#585c6b" /> :
-        <SunIcon size={28} color="white" fill="white"/>
-        }
-      </button>
-    )
+  const { theme, setTheme } = useTheme()
+
+  if (theme !== 'light' && theme !== 'dark') {
+    return null
+  }
+
+  return (
+    <button
+      onClick={() => theme == 'light' ? setTheme('dark') : setTheme('light')}
+      className={`absolute hover:cursor-pointer z-10 top-1 left-1 p-2 rounded-full ${theme == 'dark' ? 'bg-transparent' : 'bg-black'}`}>
+      {theme == 'light' ?
+        <MoonIcon size={28} color="transparent" fill="white" /> :
+        <SunIcon size={28} color="white" fill="white" />
+      }
+    </button>
+  )
 }
